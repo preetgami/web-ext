e2e04467651143b5b64cb5ed08cb58d6de84cfc3
preetgami <preetgami20@gmail.com>
2023-07-13 17:20:35 -0600

    weather

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ fetch("https://api.quotable.io/random")
 .catch(error=>{
     console.log("Error fetching quote:", error);
 })
+
+
+fetch("https://api.open-meteo.com/v1/forecast?latitude=53.5501&longitude=-113.4687&hourly=temperature_2m")
+.then(data=>data.json())
+.then(data=>{
+
+        const weather = data.hourly
+
+        const temp = weather.temperature_2m
+
+        let w = temp[0]
+        console.log(w)
+
+        const windspeed = weather.windspeed
+        const weatherElement = document.getElementById("weather");
+        weatherElement.innerHTML =w
+
+})
+.catch(error=>{
+    console.log("Error fetching weather:", error);
+
+}
+
+)
